581e5e627ba9291d80e7e50f1aad3ef5b66a5495
jstorra <131458634+jstorra@users.noreply.github.com>
2023-10-08 21:13:39 -0500

    feat: :sparkles: Añadiendo funciones
    Se añaden funciones para hacer solicitudes, validaciones y ademas se agrega un CRUD para modularizar todas las solicitudes y poder llamarse desde cualquier archivo javascript de una forma mas sencilla.
    
    (pack #1)

---
+++ functions/solicitudes.js
@@ -0,0 +1,63 @@
+import uri from "../config.js";
+import { validacionDato, validacionObj } from "../functions/validaciones.js";
+const config = { headers: { "content-type": "application/json" } };
+
+const getAll = async ({ endpoint } = {}) => {
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.' }
+    const res = await (await fetch(`${uri}${endpoint}`)).json()
+    const data = res.map((e) => Object.keys(e).length > 0 ? e: { message: 'No hay datos para mostrar.' })
+    return data
+}
+
+const getOne = async ({ id, endpoint } = {}) => {
+    if (!id) return { status: 400, message: 'Envía el Id por favor.' }
+    if (typeof id !== 'number') return { status: 400, message: 'El Id tiene un formato inválido.' }
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.' }
+    let res = await fetch(`${uri}${endpoint}${id}`)
+    if (res.status === 404) return { status: res.status, message: res.statusText }
+    res = await res.json()
+    return Object.keys(res).length > 1 ? res : { message: 'No hay datos para mostrar.' }
+}
+
+const post = async ({ obj, tabla = {}, endpoint } = {}) => {
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
+    let body = {};
+    try {
+        validacionObj(obj)
+        validacionObj(tabla)
+        Object.entries(tabla).forEach((e) => Object.assign(body,validacionDato({ nombreCampo: e[0], valor: obj[e[0]], tipoEsperado: e[1] })));
+    } catch (error) { return { status: 400, message: error.message } }
+    config.method = "POST";
+    config.body = JSON.stringify(body);
+    let res = await fetch(`${uri}${endpoint}`, config);
+    if (res.status === 201 || res.status === 404) return { status: res.status, message: res.statusText }
+}
+
+const putOne = async ({id, obj, tabla = {}, endpoint } = {}) => {
+    if (!id) return { status: 400, message: 'Envía el Id por favor.'}
+    if (typeof id !== 'number') return { status: 400, message: 'El Id tiene un formato inválido.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.' }
+    let body = {};
+    try {
+        validacionObj(obj)
+        validacionObj(tabla)
+        let data = await getOne({ id, uri, endpoint })
+        obj = {...data, ...obj}
+        Object.entries(tabla).forEach((e) => Object.assign(body,validacionDato({ nombreCampo: e[0], valor: obj[e[0]], tipoEsperado: e[1] })));
+    } catch (error) { return { status: 400, message: error.message } }
+    config.method = "PUT";
+    config.body = JSON.stringify(body);
+    let res = await fetch(`${uri}${endpoint}${id}`, config);
+    if (res.status === 200 || res.status === 404) return { status: res.status, message: res.statusText }
+}
+
+const deleteOne = async ({ id, endpoint } = {}) => {
+    if (!id) return { status: 400, message: 'Envía el Id por favor.'}
+    if (typeof id !== 'number') return { status: 400, message: 'El Id tiene un formato inválido.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
+    config.method = "DELETE";
+    let res = await fetch(`${uri}${endpoint}${id}`, config);
+    if (res.status === 200 || res.status === 404) return { status: res.status, message: res.statusText }
+}
+
+export default { getAll, getOne, post, putOne, deleteOne }
---
+++ functions/validaciones.js
@@ -0,0 +1,12 @@
+export const validacionDato = ({ nombreCampo, valor, tipoEsperado }) => {
+    if (valor.constructor.name.toLowerCase() === "string") valor = valor.trim()
+    if (valor.constructor.name.toLowerCase() === "array" && valor.length === 0) throw new Error(`Debes llenar el campo ${nombreCampo.toUpperCase()}.`)
+    if (nombreCampo === "email" && !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(valor)) throw new Error('El correo no es válido.')
+    if (!valor) throw new Error(`El campo ${nombreCampo.toUpperCase()} no está definido.`);
+    if (valor.constructor.name.toLowerCase() !== tipoEsperado.toLowerCase()) throw new Error(`El campo ${nombreCampo.toUpperCase()} tiene un formato inválido.`);
+    return { [nombreCampo]: valor };
+}
+
+export const validacionObj = (obj) => {
+  if (obj.constructor.name.toLowerCase() !== "object" || Object.keys(obj).length === 0) throw new Error('Usuario envie datos por favor.');
+}
